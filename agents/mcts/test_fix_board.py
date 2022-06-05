d370e0e2b7d6ce42f72ac61f94a4650e613be2f8
James Ku <jameskutw@gmail.com>
2022-06-05 10:02:55 +0800

    Add test_fix_board

---
+++ agents/mcts/test_fix_board.py
@@ -0,0 +1,19 @@
+import numpy as np
+from mcts import find_best_path_mcts2
+from ppad.pad.game import PAD
+board = np.array([[0, 4, 1, 4, 5, 5],[3, 5, 2, 2, 3, 4], [
+                 5, 3, 5, 4, 2, 0], [1, 1, 5, 3, 4, 2], [3, 0, 0, 1, 4, 3]])
+
+finger = np.array([1, 4])
+MIN_LEN = 20
+SIMS = 5000
+C = 1
+
+env = PAD(board=board, finger=finger)
+
+path = find_best_path_mcts2(env.board, env.finger, min_path_len=MIN_LEN, total_simulations=SIMS, C=C)
+print(path)
+for action in path:
+    env.step(env.action_to_num[action])
+print('Rendering gif...')
+env.visualize(f"mcts/sim_without_min_len_{MIN_LEN}_{SIMS}.gif")
